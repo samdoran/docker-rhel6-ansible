35e305f23782c918100e3bc855c2100d45156266
Sam Doran <sdoran@redhat.com>
2019-06-14 12:38:54 -0400

    Add option to build script

--- buildpush.sh
+++ buildpush.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo "Building container image"
 docker build --no-cache -t quay.io/samdoran/rhel6-ansible --build-arg RHSM_USERNAME=$RHSM_USERNAME --build-arg RHSM_PASSWORD=$RHSM_PASSWORD --build-arg RHSM_POOL_ID=$RHSM_POOL_ID .
 
